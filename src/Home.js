f41024166feb19bc7cd4c2e2ce3a00f137bca180
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-01-07 21:31:35 +0530

    added box2

--- src/Home.js
+++ src/Home.js
@@ -4,31 +4,92 @@ import Slider from "./Slider";
 import Product from "./Product";
 
 function Home() {
+  const Box1 = [
+    {
+      id: 1,
+      detail: "Cushion covers, bedsheets & more",
+      image: "/assets/home1.jpg",
+    },
+    {
+      id: 2,
+      detail: "Vases, decor and more",
+      image: "/assets/home2.jpg",
+    },
+    {
+      id: 3,
+      detail: "Home storage",
+      image: "/assets/home3.jpg",
+    },
+    {
+      id: 4,
+      detail: "Lighting solutions",
+      image: "/assets/home4.jpg",
+    },
+  ];
+  const Box2 = [
+    {
+      id: 1,
+      detail: "Cushion covers, bedsheets & more",
+      image: "/assets/home1.jpg",
+    },
+    {
+      id: 2,
+      detail: "Vases, decor and more",
+      image: "/assets/home2.jpg",
+    },
+    {
+      id: 3,
+      detail: "Home storage",
+      image: "/assets/home3.jpg",
+    },
+    {
+      id: 4,
+      detail: "Lighting solutions",
+      image: "/assets/home4.jpg",
+    },
+  ];
   return (
     <div className="home px-2 bg-[#dcdede]">
       <div className="home-container flex flex-col">
         <Slider />
 
-        <div className="home-row1 flex gap-6 z-[999] m-6">
-          <div className="first-item flex flex-col justify-center items-center py-5 mb-2 bg-white">
-            <h2 className="text-[22px] font-bold">
+        <div className="home-row1 flex gap-6 z-[999] m-6 -mt-1">
+          <div className="first-item flex flex-col justify-center items-center py-2 mb-2 bg-white h-96">
+            <h2 className="text-[20px] font-bold -mt-3">
               Starts early for Prime members
             </h2>
             <div className="img p-4">
-              <img src="/assets/sale.jpg" alt="" className="w-[20rem]" />
+              <img
+                src="/assets/sale.jpg"
+                alt=""
+                className="w-[20rem] cursor-pointer"
+              />
             </div>
             <div className="trial">
-              <h2 className="-ml-[10rem] text-xs text-[#36919F] hover:text-orange-600 cursor-pointer">Start 30-day Prime FREE trial &gt;</h2>
+              <h2 className="-ml-32 text-xs text-[#36919F] hover:text-orange-600 cursor-pointer">
+                Start 30-day Prime FREE trial &gt;
+              </h2>
             </div>
           </div>
           <div className="second-item bg-white py-5 mb-2">
-            <Product/>
+            <Product
+              title="Revamp your home in style"
+              showMore="Explore all"
+              Box1={Box1}
+            />
+          </div>
+          <div className="third-item bg-white py-5 mb-2">
+            <Product title="Up to 60% off | Styles for men"
+              showMore="End of season sale"
+              Box1={Box2}/>
+          </div>
+          <div className="fourth-item bg-white py-5 mb-2">
+            {/* <Product /> */}
           </div>
         </div>
         <div className="home-row2"></div>
         <div className="home-row-3"></div>
         <div className="home-row-4"></div>
-
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from "react";
 
-function Product() {
+function Product({ title, Box1, showMore }) {
   return (
-    <div className='product'>
-        <div className="product-head">
-            <h2>Keep shopping for</h2>
-        </div>
-        <div className="product-items">
-            
-        </div>
-        <div className="final-text">
-            <h2>View your browsing history</h2>
-        </div>
+    <div className="product h-80 w-[22rem]">
+      <div className="product-head py-5 mb-2 px-4">
+        <h2 className="text-[20px] font-bold -mt-3">{title}</h2>
+      </div>
+      <div className="product-items p-4 grid grid-cols-2 gap-3 -mt-7 cursor-pointer">
+        {Box1.map((val, index) => (
+          <div key={index}>
+            <img src={val.image} alt="" />
+            <h2 className="text-xs tracking-wide">{val.detail}</h2>
+          </div>
+        ))}
+      </div>
+      <div className="final-text">
+        <h2 className="ml-4 text-xs text-[#36919F] hover:text-orange-600 cursor-pointer">
+          {showMore}
+        </h2>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
