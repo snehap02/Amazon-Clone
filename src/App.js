81e7e2840964876dd200e92c2147b923921210d6
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-01-11 21:42:20 +0530

    started new section

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import SubHeader from './SubHeader';
-import Home from './Home';
-import Footer from './Footer';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import SubHeader from "./SubHeader";
+import Home from "./Home";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App w-full min-h-[100vh]">
-      {/* <h1 className='text-center'>hello</h1> */}
-      {/* header */}
-      <Header/>
-      <SubHeader/>
-      {/* home */}
-      <Home/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="app w-full min-h-[100vh]">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <SubHeader />
+            <h1>I am the checkout page</h1>
+            <Footer />
+          </Route>
+          <Route path="/">
+            <Header />
+            <SubHeader />
+            <Home />
+            <Footer />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
